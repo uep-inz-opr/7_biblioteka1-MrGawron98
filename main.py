14c7652a9af7061e22da4561f29ea6ba91ab2f48
MrGawron <55848064+MrGawron@users.noreply.github.com>
2022-05-21 22:21:47 +0200

    last change

--- main.py
+++ main.py
@@ -1,127 +1,41 @@
 class Library(list):
 
     def __init__(self):
-        super(Library, self).__init__()
+        super().__init__()
 
-    def add_book(self, author, title, year, state = "wolny"):
-        self.append({"author": author, "title": title, "year": year, "state": state, "reader": "NN"})
+    def add_book(self, book):
+        self.append(book)
 
-    def get_uniques(self):
-        return list(set([(book['title'], book['author']) for book in self]))
+    def print_get_count_books(self):
+        result = sorted(set([(book.title, book.author, self.count(book)) for book in self]), key = lambda x: x[0])
+        for book in result:
+            print(book)
 
-    def get_uniques_count(self):
-        uniques = self.get_uniques()
-        result = []
-        for book in uniques:
-            count = []
-            for e in self:
-                if e['title'] == book[0] and e['author'] == book[1]:
-                    count.append(e)
-            result.append((book[0], book[1], len(count)))
-        return sorted(result, key = lambda x: x[0])
+
+class Book:
+
+    def __init__(self, title, author, year, reader = "NN"):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.reader = reader
+
+    def __eq__(self, other):
+        if isinstance(other, Book):
+            return self.title == other.title and self.author == other.author
+        return False
 
 bib = Library()
-
 n = int(input())
 
 for i in range(n):
-    book = eval(input())
-    title = book[0]
-    author = book[1]
-    year = book[2]
-    bib.add_book(author, title, year)
+    line = eval(input())
+    title = line[0]
+    author = line[1]
+    year = line[2]
+    book = Book(title, author, year)
+    bib.add_book(book)
 
-for book in bib.get_uniques_count():
-    print(book)
+bib.print_get_count_books()
 
-    # def __init__(self):
-    #     self.core = dict()
-    #
-    # def add_book(self, author, title, year, state = "wolny", reader = "NN"):
-    #     #if author not in self.core:
-    #     #    self.core[author] = []
-    #     self.core[author].append({"title":title, "year":year, "state":state, "reader":reader})
-    #
-    # def get_size(self):
-    #     count = 0
-    #     for author in self.core:
-    #             count += len(self.core[author])
-    #     return count
-    #
-    # def get_library(self):
-    #     list = []
-    #     for author in self.core:
-    #         for book in self.core[author]:
-    #             book["author"] = author
-    #             list.append(book)
-    #     return list
-    #
-    # def get_count_books(self):
-    #     uniques = []
-    #     for book in self.get_library():
-    #         uniques.append((book['title'], book['author']))
-    #     uniques_set = set(uniques)
-    #     result = []
-    #     for book in uniques_set:
-    #         i = uniques.count(book)
-    #         result.append((book[0], book[1], i))
-    #     result = sorted(result, key = lambda x: x[0])
-    #     for x in result:
-    #         print(x)
 
-# class Biblioteka:
-#
-#     def __init__(self, lista_ksiazek = [], egzemplarze = []):
-#         self.lista_ksiazek = lista_ksiazek
-#         self.egzemplarze = egzemplarze
-#
-#     def dodaj_egzemplarz_ksiazki(self, tytul, autor, rok):
-#         k1 = Ksiazka(tytul, autor)
-#         if k1 not in self.lista_ksiazek:
-#             self.lista_ksiazek.append(k1)
-#         e1 = Egzemplarz(rok_wydania=rok, tytul=tytul, autor=autor)
-#         self.egzemplarze.append(e1)
-#         for ksiazka in self.lista_ksiazek:
-#             if ksiazka == k1:
-#                 ksiazka.ile += 1
-#
-#     def wypisz_ksiazki(self):
-#         result = []
-#         for e in self.lista_ksiazek:
-#             wynik = (e.tytul, e.autor, self.egzemplarze.count(e))
-#             result.append(wynik)
-#
-#         result = sorted(result, key = lambda x: x[0])
-#         for r in result: print(r)
-#
-#
-# class Ksiazka:
-#
-#     def __init__(self, tytul, autor, ile = 0):
-#         self.tytul = tytul
-#         self.autor = autor
-#         self.ile = ile
-#
-#     def __eq__(self, other):
-#         if isinstance(other, Ksiazka):
-#             return self.tytul == other.tytul and self.autor == other.autor
-#         return False
-#
-# class Egzemplarz(Ksiazka):
-#
-#     def __init__(self, rok_wydania, tytul, autor, wypozyczony=False):
-#         super().__init__(tytul, autor)
-#         self.rok_wydania = rok_wydania
-#         self.wypozyczony = wypozyczony
-#
-#
-# ile = int(input())
-# for i in range(ile):
-#     query = eval(input())
-#
-#     bib = Biblioteka()
-#     bib.dodaj_egzemplarz_ksiazki(query[0], query[1], query[2])
-#
-# bib.wypisz_ksiazki()
-#
-#
